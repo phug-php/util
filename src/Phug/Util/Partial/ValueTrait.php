93938e0793a690ab25b9b7b31d947edc539c8ce6
KyleKatarn <kylekatarnls@gmail.com>
2017-02-14 13:22:52 +0100

    Add hasStaticValue() to ValueTrait

--- src/Phug/Util/Partial/ValueTrait.php
+++ src/Phug/Util/Partial/ValueTrait.php
@@ -21,6 +21,22 @@ trait ValueTrait
     }
 
     /**
+     * @return boolean
+     */
+    public function hasStaticValue()
+    {
+        if (is_string($this->value)) {
+            $tokens = token_get_all('<?php '.$this->value);
+            return
+                count($tokens) === 2 &&
+                is_array($tokens[1]) &&
+                in_array($tokens[1][0], [T_CONSTANT_ENCAPSED_STRING, T_DNUMBER, T_LNUMBER]);
+        }
+
+        return false;
+    }
+
+    /**
      * @param mixed $value
      */
     public function setValue($value)
