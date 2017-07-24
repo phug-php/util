c8d08884b270c014b180fe85e60ca788426fbf34
KyleKatarn <kylekatarnls@gmail.com>
2017-07-25 00:26:20 +0200

    Unit tests for sand box buffer

--- tests/Phug/Util/SandBoxTest.php
+++ tests/Phug/Util/SandBoxTest.php
@@ -14,30 +14,56 @@ class SandBoxTest extends \PHPUnit_Framework_TestCase
     /**
      * @covers ::__construct
      * @covers ::getResult
+     * @covers ::getBuffer
+     * @covers ::outputBuffer
      */
     public function testSuccess()
     {
         $sandBox = new SandBox(function () {
+            echo 'bar';
+
             return 'foo';
         });
 
         self::assertSame(null, $sandBox->getThrowable());
         self::assertSame('foo', $sandBox->getResult());
+        self::assertSame('bar', $sandBox->getBuffer());
+
+        ob_start();
+        $sandBox->outputBuffer();
+        $contents = ob_get_contents();
+        ob_end_clean();
+
+        self::assertSame('bar', $contents);
+
+        ob_start();
+        $sandBox->outputBuffer();
+        $contents = ob_get_contents();
+        ob_end_clean();
+
+        self::assertSame('', $contents);
+
     }
 
     /**
      * @covers ::__construct
      * @covers ::getThrowable
+     * @covers ::getBuffer
      */
     public function testError()
     {
         $sandBox = new SandBox(function () {
-            return 5 / 0;
+            echo 'foo';
+            $a = 5 / 0;
+            echo 'bar';
+
+            return $a;
         });
 
         self::assertInstanceOf(Exception::class, $sandBox->getThrowable());
         self::assertContains('Division by zero', $sandBox->getThrowable()->getMessage());
         self::assertSame(null, $sandBox->getResult());
+        self::assertSame('foo', $sandBox->getBuffer());
     }
 }
 //@codingStandardsIgnoreEnd
